c44bcef959bfad9a2dcbdeccb49ce97401a15ca0
Nitkarsh Chourasia <playnitkarsh@gmail.com>
2023-01-09 21:00:12 +0530

    newCommits @ 2023-01-09-21:00:11

--- Java/originalSeries/src/Main.java
+++ Java/originalSeries/src/Main.java
@@ -2,5 +2,12 @@ package com.NitkarshChourasia;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Pallavi, How are you? Do you have any doubts regarding to me?");
+
+    }
+}
+
+class Hello{
+    public static void chicken(String[] args){
+        System.out.println("Chal na laude");
     }
 }
