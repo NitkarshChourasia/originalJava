3d01dd0dad52da1bae3cb70616a99a9894362e1a
Nitkarsh Chourasia <playnitkarsh@gmail.com>
2023-01-10 14:00:07 +0530

    newCommits @ 2023-01-10-14:00:06

--- Java/originalSeries/src/Main.java
+++ Java/originalSeries/src/Main.java
@@ -1,9 +1,44 @@
 package com.NitkarshChourasia;
+
+import java.text.NumberFormat;
+
 public class Main {
     public static void main(String[] args) {
+//        scanner scanned = new scanner();
+//
+//        System.out.print("Please enter your principal amount: ");
+//        double principal = scanned.next();
+//        System.out.println();
+//
+//        System.out.print("Please enter your rate of interest: ");
+//        double rate = scanned.next();
+//        System.out.println();
+//
+//        System.out.print("Please enter your loan duration in years: ");
+//        double time = scanned.next();
+//        System.out.println();
+//
+//        double time = time * 12;
+//        double rate = (rate / 100) / 12;
+//        double monthlyPayment = principal(rate(math.pow((1 + rate), time))) / (math.pow((1 + rate), time) - 1);
+//        double monthyPayment = (principal * rate * Math.pow((1 + rate), time)) / (Math.pow((1 + rate), time) - 1);
+//        NumberFormat currency = NumberFormat.getCurrencyInstance(Locale.INDIA);
+//        String result = currency.format(monthlyPayment);
+//        System.out.println("Your monthly payment is: " + result);
+        // Build a mortgage calculator.
         System.out.print("Please enter your principal amount: ");
+        int principal = (int) Console.readNumber();
+        System.out.println();
         System.out.print("Please enter your rate of interest: ");
-        System.out.print("Please enter your loan duration: ");
-        
+        float rate = (float) Console.readNumber();
+        System.out.println();
+        System.out.print("Please enter your loan duration in years: ");
+        int time = (int) Console.readNumber();
+        System.out.println();
+        int timeInMonths = time * 12;
+        float rateInMonths = (rate / 100) / 12;
+        double monthlyPayment = principal * (rateInMonths * Math.pow((1 + rateInMonths), timeInMonths)) / (Math.pow((1 + rateInMonths), timeInMonths) - 1);
+        String result = NumberFormat.getCurrencyInstance().format(monthlyPayment);
+        System.out.println("Your monthly payment is: " + result);
     }
 }
